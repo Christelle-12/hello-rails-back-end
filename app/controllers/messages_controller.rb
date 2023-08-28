6c3e655406da63af29c92978a5f8d5fc43f0b03a
Chritelle-12 <mariechristellenirere@gmail.com>
2023-08-28 13:54:45 +0200

    filx linters

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class MessagesController < ApplicationController
-    def index
-        random_greeting = Message.order('RANDOM()').first
-        render json: { greetings: random_greeting.content }
-      end
+  def index
+    random_greeting = Message.order('RANDOM()').first
+    render json: { greetings: random_greeting.content }
+  end
 end
